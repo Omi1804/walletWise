d1709a8a09e396285423ac902c467260be3308f1
Om Nigam <om2021064@akgec.ac.in>
2024-03-16 18:03:30 +0530

    adding backend logic

--- backend/src/middlewares/Auth.ts
+++ backend/src/middlewares/Auth.ts
@@ -1,8 +1,39 @@
 import jwt from "jsonwebtoken";
+const secret = process.env.SEC_KEY;
 import { Request, Response, NextFunction } from "express";
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: string; // Declare userId property on Request object
+    }
+  }
+}
 
 export const authenticateUser = (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  const authHeader = req.header("Authorization");
+  try {
+    if (authHeader) {
+      const token = authHeader.split("")[0];
+      if (token && secret) {
+        const payload = jwt.verify(token, secret) as { _id: string };
+        console.log("from middleware : ", payload);
+        req.userId = payload._id;
+        next();
+      } else {
+        return res
+          .status(401)
+          .json({ message: "Token or secret not defined." });
+      }
+    } else {
+      return res.status(403).json({ message: "User not authenticated!" });
+    }
+  } catch (error) {
+    console.error("Error in authentication middleware:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
